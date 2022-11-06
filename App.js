315336fe15b047a0d564173efe66f5dd1465dc5d
bobbyzhong <3bobbyzhong3@gmail.com>
2022-11-06 01:48:18 -0800

    init

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { useFonts } from "expo-font";
+import HomeScreen from "./screens/home.js";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const getFonts = () => {
+    const [fontsLoaded] = useFonts({
+        "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+        "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+    });
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const navigator = createStackNavigator(
+    {
+        Home: HomeScreen,
+    },
+    {
+        initialRouteName: "Home",
+        defaultNavigationOptions: {
+            title: "App",
+            headerShown: false,
+        },
+    }
+);
+
+export default createAppContainer(navigator);
